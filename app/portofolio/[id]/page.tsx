632bc9e22af157765f7d41d92e023a286ddded42
eyoram <esarama22@gmail.com>
2025-06-25 15:03:09 +0700

    fixed-type-params-portofolio-page

--- app/portofolio/[id]/page.tsx
+++ app/portofolio/[id]/page.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import Image from "next/image";
 import { arrayPorto } from "../../../data/portofolio";
 
-type Params = {
-  id: string;
-};
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-export default async function ProjectDetail({
-  params,
-}: {
-  params: Params;
-}) {
+export default function ProjectDetail({ params }: PageProps) {
   const { id } = params;
   const porto = arrayPorto.find((p) => p.id === parseInt(id));
 
-  if (!porto) return <div>Project not found</div>;
+  if (!porto) {
+    return <div className="text-white p-4">Project not found</div>;
+  }
 
   return (
     <div className="container mx-auto p-4 bg-gray-950">
@@ -27,13 +27,12 @@ export default async function ProjectDetail({
         </Link>
         <p className="font-semibold text-sm text-blue-400">{porto.category}</p>
         <h1 className="text-2xl font-bold text-white">{porto.name}</h1>
-        <p className="mb-4">{porto.description}</p>
+        <p className="mb-4 text-white">{porto.description}</p>
         <Image
           alt={porto.name}
           src={porto.image}
           width={500}
           height={50}
-          className=""
           unoptimized
         />
       </div>
